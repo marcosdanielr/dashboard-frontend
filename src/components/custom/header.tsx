db8c7f7769471758b31e2fa3e4849dfca31a4c9f
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-02-22 02:06:37 -0300

    fix: header tag

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -4,11 +4,11 @@ import logo from '@assets/icons/logo.svg'
 
 export const Header = () => {
   return (
-    <div className='w-full h-[4.819375rem] px-5 bg-white flex justify-between items-center'>
+    <header className='w-full h-[4.819375rem] px-5 bg-white flex justify-between items-center'>
       <img src={logo} alt="Flugo logo" className='cursor-pointer' /> 
 
       <User />
       
-    </div>
+    </header>
   )
 }
