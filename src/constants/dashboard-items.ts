df21298a38913a9faed7512fb76073a869932180
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-02-22 23:46:47 -0300

    fix: percent description in dashboard cards

--- src/constants/dashboard-items.ts
+++ src/constants/dashboard-items.ts
@@ -7,19 +7,19 @@ export const dashboardCards = (percents: number[]) => {
   const cards = [
     {
       title: 'Receita',
-      description: `+${percents[0]} em relação ao mês passado`,
+      description: `+${percents[0]}% em relação ao mês passado`,
       value: numberToBRL(25000),
       icon: MdAttachMoney
     },
     {
       title: 'Clientes',
-      description: `+${percents[1]} em relação ao mês passado`,
+      description: `+${percents[1]}% em relação ao mês passado`,
       value: `+${120}`,
       icon: HiUsers
     },
     {
       title: 'Vendas',
-      description: `+${percents[2]} em relação ao mês passado`,
+      description: `+${percents[2]}% em relação ao mês passado`,
       value: `+${50}`,
       icon: MdSell
     },
